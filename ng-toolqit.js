8d3c242e5a15f47e1b7f3ec857c4ae37975a68a4
Matias Chomicki <matyax@gmail.com>
2016-01-24 17:36:35 -0300

    Add ng-toolqit code with range filter

---
+++ ng-toolqit.js
@@ -0,0 +1,30 @@
+/*
+ * ng-toolqit
+ * https://github.com/matyax/ng-toolqit
+
+ * Version: 1.0.0
+ * License: MIT
+ */
+(function(window, angular, undefined) {'use strict';
+
+
+angular.module('ngCookies', ['ng']).
+
+  filter('range', function() {
+    return function(range, quantity) {
+      if (typeof range != 'object') {
+        throw new Error('Filter input must be an empty array.');
+      }
+
+      quantity = parseInt(quantity);
+
+      for (var i = 0; i < quantity; i++) {
+        range.push(i);
+      }
+
+      return range;
+    };
+  });
+
+
+})(window, window.angular);
